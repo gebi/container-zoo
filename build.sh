866858bf9348870558f5cd440762f893b41c05ca
Michael Gebetsroither <michael@mgeb.org>
2017-06-24 00:16:50 +0200

    next

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -ex
 
 docker build $* -t local/alpine-dev alpine-dev
 docker build $* -t local/debian:stretch debian/stretch
+docker build $* -t local/debian:jessie debian/jessie
+docker build $* -t local/ubuntu:16.04 ubuntu/16.04
+docker build $* -t local/scribus scribus
---
+++ scribus/run.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+
+QT_X11_NO_MITSHM=1 scribus
