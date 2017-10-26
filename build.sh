864d0a93be2ea7fa4975af13a0aa6f40bb0f4091
Michael Gebetsroither <michael@mgeb.org>
2017-10-26 04:33:06 +0200

    remove scribus from default build

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ docker build $* -t local/alpine-dev alpine-dev
 docker build $* -t local/debian:stretch debian/stretch
 docker build $* -t local/debian:jessie debian/jessie
 docker build $* -t local/ubuntu:16.04 ubuntu/16.04
-docker build $* -t local/scribus scribus
+# not used anymore
+#docker build $* -t local/scribus scribus
