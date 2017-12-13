6e8f3aaae535babec558ee03a93254c397b2fa6d
Michael Gebetsroither <michael@mgeb.org>
2017-12-14 00:48:01 +0100

    update debian images

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 set -ex
 
+parallel docker pull -- alpine:latest debian:jessie debian:stretch ubuntu:xenial
+
+# base images
 docker build $* -t local/alpine-dev alpine-dev
 docker build $* -t local/debian:stretch debian/stretch
 docker build $* -t local/debian:jessie debian/jessie
 docker build $* -t local/ubuntu:16.04 ubuntu/16.04
+
 # not used anymore
 #docker build $* -t local/scribus scribus
