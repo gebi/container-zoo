43c2f32d988304e2f03758039939b2c76d826188
Michael Gebetsroither <michael@mgeb.org>
2018-03-02 22:57:13 +0100

    next

--- ttest.py
+++ ttest.py
@@ -2,13 +2,14 @@
 
 import os
 import sys
+import yaml
 from jinja2 import Environment, FileSystemLoader
 
 DEBUG=False
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
     autoescape=False,
-    loader=FileSystemLoader(os.path.join(PATH, './')),
+    loader=FileSystemLoader(os.path.join(PATH, 'debian')),
     trim_blocks=False)
 
 # example context:
@@ -16,10 +17,9 @@ TEMPLATE_ENVIRONMENT = Environment(
 #        'IMAGE_BASE': 'debian:jessie',
 #        'IMAGE_DIST_NAME': 'jessie',
 #    }
-def create(context, output):
+def create(docker_template, context, output):
     os.makedirs(os.path.dirname(output), exist_ok=True)
-    tname = "debian/Dockerfile.j2"
-    html = TEMPLATE_ENVIRONMENT.get_template(tname).render(context)
+    html = TEMPLATE_ENVIRONMENT.get_template(docker_template).render(context)
     if DEBUG:
         sys.stdout.write(html)
         sys.stdout.write('\n')
@@ -29,29 +29,28 @@ def create(context, output):
         f.write('\n')
 
 def main():
-    full_debian_release_list = [ \
-            "wheezy",
-            "wheezy-backports",
-            "jessie",
-            "jessie-backports",
-            "stretch",
-            "stretch-backports",
-            "buster",
-            "buster-backports",
-            "sid",
-            ]
-    debian_release_list = [ \
-            "wheezy",
-            "jessie",
-            "stretch",
-            "buster",
-            "sid",
-            ]
-    instances = [{'IMAGE_BASE': "debian:%s" %(i), 'IMAGE_DIST_NAME': i} for i in debian_release_list]
-    for c in instances:
-        create(c, os.path.join(PATH, "debian", c['IMAGE_DIST_NAME'], "Dockerfile"))
+    debian_release_list = [ "wheezy", "jessie", "stretch", "buster", "sid"]
+    instances = [{'IMAGE_BASE': "debian:%s" %(i), 'dist_name': i} for i in debian_release_list]
+    for context in instances:
+        create("debian/Dockerfile.j2", context, os.path.join(PATH, "debian", c['dist_name'], "Dockerfile"))
+
+def main2():
+    tname = "build.j2"
+    context = {}
+    build_yaml = TEMPLATE_ENVIRONMENT.get_template(tname).render(context)
+    build = yaml.safe_load(build_yaml)
+    # create default vars
+    for (k,v) in build.items():
+        e = v.get('env', dict)
+        e['IMAGE_BASE'] = v['base']
+        e['IMAGE_NAME'] = k
+    sys.stdout.write(yaml.dump(build, default_flow_style=False))
+    for (k,v) in build.items():
+        create(v['dockerfile'], v['env'], os.path.join(PATH, "debian", k, "Dockerfile"))
+
 
 ########################################
 
 if __name__ == "__main__":
-    main()
+    #main()
+    main2()
