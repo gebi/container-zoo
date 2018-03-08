28c31a9268044a0bae1356c8cdbc627ab6c27af1
Michael Gebetsroither <michael@mgeb.org>
2018-03-08 20:08:56 +0100

    next

--- build.py
+++ build.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 
+import sys
 import os
 import subprocess
+import click
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+DEBUG=None
+
+# container graph
+#   container_name (from fs) -> [ base (from Dockerfile) ]
+cg = {}
+# container graph reverse
+#   container_base_name -> [ container depending on this base ]
+cgr = {}
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'debian')),
+    trim_blocks=False)
+
 
 # walk {{{
 def walk(path, filter):
@@ -51,41 +71,80 @@ def get_base_images():
         if i not in cg:
             yield i
 
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    #click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+    global DEBUG
+    DEBUG=debug
 
-# container graph
-#   container_name (from fs) -> [ base (from Dockerfile) ]
-cg = {}
-# container graph reverse
-#   container_base_name -> [ container depending on this base ]
-cgr = {}
+@cli.command()
+@click.option('--update/--no-update', default=False)
+def upgrade_base(update): # {{{
+    '''Upgrade base docker images'''
+    for i in walk('.', dockerfilter):
+        #print(i)
+        container_base = getbase(i)
+        container_name = get_container_name(i)
+        cg[container_name] = container_base
 
-for i in walk('.', dockerfilter):
-    #print(i)
-    container_base = getbase(i)
-    container_name = get_container_name(i)
-    cg[container_name] = container_base
+    # create reverse mapping
+    print("Local Container:")
+    for container_name,base_set in cg.items():
+        print("\t%s -> %s" %(container_name, base_set))
+        for base in base_set:
+            i = cgr.get(base, set())
+            i.add(container_name)
+            cgr[base] = i
 
-# create reverse mapping
-print("Local Container:")
-for container_name,base_set in cg.items():
-    print("\t%s -> %s" %(container_name, base_set))
-    for base in base_set:
-        i = cgr.get(base, set())
-        i.add(container_name)
-        cgr[base] = i
+    print("\nPrint dependencies:")
+    for k in sorted(cgr):
+        v = cgr[k]
+        print("\t%s <- %s" %(k,sorted(v)))
 
-print("\nPrint dependencies:")
-for (k,v) in cgr.items():
-    print("\t%s <- %s" %(k,v))
+    print("\nBase images:")
+    for i in sorted(get_base_images()):
+        print('\t%s'% i)
 
-print("\nBase images:")
-for i in sorted(get_base_images()):
-    print('\t%s'% i)
+    print("\nUpdating base images:")
+    to_update = sorted(filter(lambda x: x.find('/') == -1, get_base_images()))
+    cmd = ["parallel", "docker", "pull", "--"] + to_update
+    print("\t%s" % cmd)
+    if update:
+        subprocess.check_call(cmd)
+    #print(cg)
+# }}}
 
-print("\nUpdating base images:")
-to_update = sorted(filter(lambda x: x.find('/') == -1, get_base_images()))
-cmd = ["parallel", "docker", "pull", "--"] + to_update
-print("\t%s" % cmd)
-subprocess.check_call(cmd)
+def create(docker_template, context, output):
+    html = TEMPLATE_ENVIRONMENT.get_template(docker_template).render(context)
+    if DEBUG:
+        os.makedirs(os.path.dirname(output), exist_ok=True)
+        sys.stdout.write("# %s (%s)\n" %(docker_template, output))
+        sys.stdout.write(html)
+        sys.stdout.write('\n\n')
+        return
+    with open(output, 'w') as f:
+        f.write(html)
+        f.write('\n')
 
-#print(cg)
+@cli.command()
+def build(): # {{{
+    '''Build all images'''
+    tname = "build.j2"
+    context = {}
+    build_yaml = TEMPLATE_ENVIRONMENT.get_template(tname).render(context)
+    build = yaml.safe_load(build_yaml)
+    # create default vars
+    for (k,v) in build.items():
+        e = v.get('env', dict)
+        e['IMAGE_BASE'] = v['base']
+        e['IMAGE_NAME'] = k
+    sys.stdout.write("# %s (%s)\n" %(tname, "build"))
+    sys.stdout.write(yaml.dump(build, default_flow_style=False))
+    sys.stdout.write("\n\n")
+    for (k,v) in build.items():
+        create(v['dockerfile'], v['env'], os.path.join(PATH, "debian", k, "Dockerfile"))
+# }}}
+
+if __name__ == '__main__':
+    cli()
