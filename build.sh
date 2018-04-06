70b4bdfe584dda95a61742af99b3454811b80555
Michael Gebetsroither <michael@mgeb.org>
2018-04-06 23:59:21 +0200

    build.sh: add ubuntu 14.04

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ docker build $* -t local/alpine-dev alpine-dev
 docker build $* -t local/debian:stretch debian/stretch
 docker build $* -t local/debian:jessie debian/jessie
 
+docker build $* -t local/ubuntu:14.04 ubuntu/14.04
+docker tag local/ubuntu:14.04 local/ubuntu:trusty
+
 docker build $* -t local/ubuntu:16.04 ubuntu/16.04
 docker tag local/ubuntu:16.04 local/ubuntu:xenial
 
