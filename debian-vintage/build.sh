5bf9ebf924503195804fc5e5f092cbb9cafa2db8
Michael Gebetsroither <michael@mgeb.org>
2018-08-09 16:29:33 +0200

    debian-vintage/build: make test output more clear and propagate exit code for tests

--- debian-vintage/build.sh
+++ debian-vintage/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CMD=$1
+ret=0
 
 for i in potato woody sarge etch lenny squeeze; do
     if [[ $CMD == "build" ]]; then
@@ -8,6 +9,13 @@ for i in potato woody sarge etch lenny squeeze; do
         docker build -t gebi/debian-vintage:$i $i/docker
         docker push gebi/debian-vintage:$i
     elif [[ $CMD == "test" ]]; then
+        echo -en "$i:\t"
         docker run -it --rm gebi/debian-vintage:$i /usr/bin/env cat /etc/debian_version
+        rettmp=$?
+        if [[ $rettmp != 0 ]]; then
+            ret=1
+        fi
     fi
 done
+
+exit $ret
